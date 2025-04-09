2f7749d45b147ac9350822567af78c21cb0e677c
jayaspassion <jayaspassion@gmail.com>
2025-04-09 15:49:28 -0400

    HTML Structure

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <div>Logo</div>
+    <div>Doormat Navigation</div>
+    <div>Contact</div>
+    <div>Social Media Link</div>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from "../assets/Logo.svg"; // Adjust the path as necessary
+
+function Nav() {
+  return (
+    <nav>
+      <img src={logo} alt="Logo" />
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#reservation">Reservation</a></li>
+        <li><a href="#order-online">Order Online</a></li>
+        <li><a href="#login">Login</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default Nav;
